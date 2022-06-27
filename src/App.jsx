76f767d361f19e671fa9ac6f57aea3e5972f1809
Re-Dnor <MokshinRE@yandex.ru>
2022-06-27 17:40:43 +0500

    add themes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,38 @@ import Form from "./Components/Form.jsx";
 import Content from "./Components/Content.jsx"
 import getData from './Utils/getData.js'
 import parse from "./Utils/parser.js";
+import ThemeContext from "./Components/Context/ThemeContext.js";
+
+
+const themes = [
+  {
+    name: 'black',
+    className: 'bg-dark',
+    id: 0
+  },
+  {
+    name: 'blue',
+    className: 'bg-primary',
+    id: 1,
+  },
+  {
+    name: 'yellow',
+    className: 'bg-warning',
+    id: 2
+  },
+  {
+    name: 'red',
+    className: 'bg-danger',
+    id: 3
+  }
+];
 
 const App = (props) => {
-  const [weatherData, setWeatherData] = useState({})
+  const [weatherData, setWeatherData] = useState({});
   const [errorFeedback, setErrorFeedback] = useState('');
-  const handleSubmitForm = (value, setStateCity) => {
+  const [currentTheme, setStateTheme] = useState(themes[0]);
 
+  const handleSubmitForm = (value, setStateCity) => {
     getData(value)
     .then((xml) => {
       const { contents } = xml.data;
@@ -39,8 +65,10 @@ const App = (props) => {
   return (
     <main className="d-flex min-vh-100 flex-column bg-dark">
       <div className="flex-grow-1 text-white">
+        <ThemeContext.Provider value={{themes, currentTheme, setStateTheme}}>
         <Form weatherData={weatherData} errorFeedback={errorFeedback}  handleSubmitForm={handleSubmitForm} />
         <Content weatherData={weatherData} />
+        </ThemeContext.Provider>
       </div>
     </main>
   )
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,9 +1,12 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
 import cn from "classnames";
+import ThemeContext from "./Context/ThemeContext";
+import ThemeSwitcher from "./ThemeSwitcher.jsx";
 
 const Form = (props) => {
   const {handleSubmitForm, errorFeedback} = props;
   const [city, setStateCity] = useState('');
+  const {currentTheme} = useContext(ThemeContext);
   const classNamaFeedback = cn('feedback m-0 position-absolute medium', errorFeedback? 'text-danger' :'text-success')
 
   const submitForm = (e) => {
@@ -16,8 +19,13 @@ const Form = (props) => {
     setStateCity(e.target.value);
   }
 
+  const {className} = currentTheme;
+
+  const classNameTheme = cn("container-fluid p-5", className)
+
   return (
-    <section className="container-fluid p-5 bg-dark">
+    <section className={classNameTheme}>
+      <ThemeSwitcher />
       <div className="row">
         <div className="col-md-10 col-lg-5 mx-auto text-white">
           <h1 className="display-5 text-center m-4">Weather</h1>
